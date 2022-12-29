39d1794f2b8c2e9230ae68324320af5a48078c35
Hwiriric <smk6270@gmail.com>
2022-12-29 16:58:16 +0900

    데이터베이스 생성 및 테이블 조회

---
+++ db/sql/create_company_db.sql
@@ -0,0 +1,21 @@
+-- create database company;
+
+-- use company;
+
+-- show tables
+
+-- desc department;
+-- desc dependent;
+-- desc employee;
+-- desc part;
+-- desc part_contain;
+-- desc project;
+-- desc project_location;
+-- desc supplier;
+-- desc supply;
+-- desc work;
+
+-- select * from department;
+-- select * from employee;
+-- select * from dependent;
+-- select * from project;
