c406025b003eac365e091dc07a9decb5f47bcf39
Hwiriric <smk6270@gmail.com>
2022-12-29 16:58:46 +0900

    가상 엔티티 생성

---
+++ db/sql/insert_values.sql
@@ -0,0 +1,18 @@
+insert into department values(1, '기획부', 1);
+insert into department values(2, '개발부', 2);
+insert into department values(3, '홍보부', 3);
+
+insert into employee values(4377, '이성태', '사장', 5000000, '서울특별시', '동대문구', '휘경동', 2);
+insert into employee values(3426, '박영권', '과장', 3000000, '서울특별시', '동대문구', '휘경동', 1);
+insert into employee values(3011, '이수민', '부장', 4000000, '서울특별시', '동대문구', '휘경동', 3);
+insert into employee values(3427, '최종철', '사원', 1500000, '서울특별시', '동대문구', '휘경동', 3);
+insert into employee values(1003, '조민희', '과장', 3000000, '서울특별시', '동대문구', '휘경동', 2);
+insert into employee values(2106, '김창섭', '대리', 2500000, '서울특별시', '동대문구', '휘경동', 2);
+insert into employee values(1365, '김상원', '사원', 1500000, '서울특별시', '동대문구', '휘경동', 1);
+
+insert into dependent values(1365, '김재중', '남자');
+insert into dependent values(3427, '박지윤', '여자');
+
+insert into project values(1, 'project-X', 10000000, 3426, '2020-12-3');
+insert into project values(2, 'project-Y', 20000000, 3011, '2020-4-15');
+insert into project values(3, 'project-Z', 30000000, 1003, '2019-3-20');
