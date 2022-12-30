96e9af9f31dd292f78c53e5cebc1ea4b0d419b14
Hwiriric <smk6270@gmail.com>
2022-12-30 10:01:16 +0900

    두 개의 root 유저 발견
    - localhost로 접근한 것이 아닌 것 같음
    - company db를 못 찾는 것이 설명됨
    - host값으로 127.0.0.1을 갖는 root의 db를 찾아봐야 할 것 같음

--- java/DJ_Project/src/main/Main.java
+++ java/DJ_Project/src/main/Main.java
@@ -1,21 +1,28 @@
 package main;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
+			// db에 접속
 			Connection conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/company", 
+					"jdbc:mysql://localhost:3306/mysql", 
 					"root",
 					"712476"
 					);
-			System.out.println("success");
+			System.out.println("success\n");
+			
+			// db에 명령어를 전달할 수 있는 객체 생성
 			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("select user, host from user");
+			while(rs.next()) {				
+				System.out.println(rs.getString("user") + "    " +
+						rs.getString("host"));
+			}
+			
+			
 		} catch(SQLException e) {
 			System.out.println("error: " + e);
 		}
