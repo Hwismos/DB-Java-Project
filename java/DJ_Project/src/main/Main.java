afb31963627c57a42cb5397270d78e0656011398
Hwiriric <smk6270@gmail.com>
2022-12-31 22:45:55 +0900

    1일 1커밋을 위한 커밋

--- java/DJ_Project/src/main/Main.java
+++ java/DJ_Project/src/main/Main.java
@@ -7,8 +7,9 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			// db에 접속
+			// jdbc:Driver 종류: //IP포트번호/DB명
 			Connection conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/mysql", 
+					"jdbc:mysql://localhost:3306/?user=company", 
 					"root",
 					"712476"
 					);
@@ -16,15 +17,14 @@ public class Main {
 			
 			// db에 명령어를 전달할 수 있는 객체 생성
 			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("select user, host from user");
+			ResultSet rs = stmt.executeQuery("select eno from employee");
 			while(rs.next()) {				
-				System.out.println(rs.getString("user") + "    " +
-						rs.getString("host"));
+				System.out.println(rs.getString("eno"));
 			}
 			
 			
 		} catch(SQLException e) {
-			System.out.println("error: " + e);
+			System.out.println("SQLException: " + e);
 		}
 	}
 
